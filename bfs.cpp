dfc43c00843791b861c5fc1ad4dd65fd8d513ae4
Ali Ahnaf Swapnil <46298019+Propo41@users.noreply.github.com>
2020-03-27 18:34:54 +0600

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,46 +1,41 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-void bfs(vector<int> graph[], int curr, int n)
+# include <bits/stdc++.h>
+using namespace std ;
+void bfs ( vector < int > graph[], int curr, int n)
 {
-    bool visited[n] = {false};
-    queue<int> g_queue;
-
-    g_queue.push(curr);
-    visited[curr]=true;
-
-    while(!g_queue.empty())
-    {
-
-        curr = g_queue.front();
-        cout << curr << " ";
-        g_queue.pop();
-        for(auto i=graph[curr].begin(); i!=graph[curr].end(); i++)
-        {
-            if(!visited[*i])
-            {
-                visited[*i] = true;
-                g_queue.push(*i);
-            }
-        }
-
-    }
-
+bool visited[n]= { false };
+queue < int > g_queue;
+g_queue.push(curr);
+visited[curr]= true ;
+while (!g_queue.empty())
+{
+for ( int i= 0 ; i<graph[curr].size(); i++)
+{
+if (!visited[graph[curr][i]])
+{
+visited[graph[curr][i]] = true ;
+g_queue.push(graph[curr][i]);
 }
-
-
-int main()
+}
+curr = g_queue.front();
+cout << curr << " " ;
+g_queue.pop();
+}
+}
+int main ()
 {
-
-    int v = 5; // number of nodes (0-4)
-
-    vector<char> graph[v];
-    cout << ((char)('A'- 17)) - '0' << endl;
-    graph[((char)('A'- 17)) - '0'].push_back('C');
-
-
-
-
-    return 0;
+int v = 8 ; // number of nodes
+vector < int > graph[v];
+int a,b;
+//taking input of either directed or undirected graph
+// adjacency list used
+while ( true )
+{
+cin >> a;
+if (a== -1 )
+break ;
+cin >> b;
+graph[a].push_back(b);
+}
+bfs(graph, 1 , v);
+return 0 ;
 }
